0a400c0a891f7daa2c6689f885f32f188fe6130a
Aditya Shrivastav <devs.aadi20@gmail.com>
2025-07-31 04:02:20 +0530

    responsive-mobile

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -31,11 +31,11 @@ function App() {
 
   return (
     <>
-      <div className="h-dvh w-full bg-gradient-to-t from-[#1d1d1d] to-[#020202] flex items-center justify-center lg:p-0 p-4">
+      <div className="h-dvh overflow-hidden w-full bg-gradient-to-t from-[#1d1d1d] to-[#020202] flex items-center justify-center lg:p-0 p-4">
         <div className="flex flex-col items-center gap-1 bg-gradient-to-t hover:from-[#182135] hover:to-[#080808] p-10 border-2 border-gray-200 rounded-xl transition-colors ease-linear duration-[800ms]">
           <div className='flex flex-col gap-14 items-center mt-6'>
             <h1 className='text-white lg:text-5xl text-4xl md:text-1xl font-medium'>Quote Deck</h1>
-            <div className='bg-gray-800 p-6 rounded-md lg:w-[25rem] w-full'>
+            <div className='bg-gray-800 p-6 rounded-md lg:w-[25rem] w-[17rem] h-full'>
               {loading ? (<h1 className='text-white text-xl text-center'>Wait Bitch...🙄</h1>) : (<h1 className='text-white text-xl'>{quote}</h1>)}
             </div>
           </div>
